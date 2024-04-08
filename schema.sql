d77ba9b73ce5cc5a107f0cf662c2878beb32b67f
Cezar Șandra <32837227+cezarsandra@users.noreply.github.com>
2024-04-08 13:22:23 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,44 +1,42 @@
 CREATE TABLE `authors` (
-  `id` INTEGER AUTO_INCREMENT,
+  `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
   `Lastname` VARCHAR(255),
   `Firstname` VARCHAR(255),
-  `books` INTEGER,
-  PRIMARY KEY (`id`, `books`)
 );
+
+CREATE TABLE `authors_books` (
+  `id` INTEGER PRIMARY KEY,
+  `author_id` INTEGER,
+  `book_id` INTEGER,
+);
+
+CREATE TABLE `books` (
+  `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
+  `photo` VARCHAR(255),
+  `title` VARCHAR(255) NOT NULL,
+  `author_id` INTEGER NOT NULL,
+  `description` TEXT COMMENT 'Content of the post',
+  -- `subscriber_id` INTEGER,
+  `is_borrowed` BOOLEAN DEFAULT FALSE,
+);
+
 
 CREATE TABLE `subscribers` (
   `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
   `Lastname` VARCHAR(255),
   `Firstname` VARCHAR(255),
-  `review` VARCHAR(255)
-);
-
-CREATE TABLE `books` (
-  `id` INTEGER AUTO_INCREMENT,
-  `photo` VARCHAR(255),
-  `title` VARCHAR(255) NOT NULL,
-  `author` INTEGER NOT NULL,
-  `description` TEXT COMMENT 'Content of the post',
-  `subscriber` INTEGER,
-  `borrowed_books` INTEGER DEFAULT 0,
-  `is_borrowed` BOOLEAN DEFAULT FALSE,
-
+  `Email` VARCHAR(255)
 );
 
 CREATE TABLE `borrowed_books` (
-  `id` INTEGER,
-  `id_subscriber` INTEGER,
-  `id_book` INTEGER,
+  `id` INTEGER PRIMARY KEY,
+  `subscriber_id` INTEGER,
+  `book_id` INTEGER,
   `subscription_date` TIMESTAMP,
   `return_date` TIMESTAMP,
-  PRIMARY KEY (`id`, `id_subscriber`, `id_book`)
 );
 
--- Add Index on Creation Date for Items Table
-CREATE INDEX idx_items_created_at ON items (created_at);
-
-ALTER TABLE `books` ADD FOREIGN KEY (`author`) REFERENCES `authors` (`id`);
-ALTER TABLE `books` ADD FOREIGN KEY (`subscriber`) REFERENCES `subscribers` (`id`);
-ALTER TABLE `borrowed_books` ADD FOREIGN KEY (`id_subscriber`) REFERENCES `subscribers` (`id`);
-ALTER TABLE `borrowed_books` ADD FOREIGN KEY (`id_book`) REFERENCES `books` (`id`);
-ALTER TABLE `books` ADD FOREIGN KEY (`borrowed_books`) REFERENCES `borrowed_books` (`id`);
+ALTER TABLE `books` ADD FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`);
+ALTER TABLE `books` ADD FOREIGN KEY (`subscriber_id`) REFERENCES `subscribers` (`id`);
+ALTER TABLE `borrowed_books` ADD FOREIGN KEY (`subscriber_id`) REFERENCES `subscribers` (`id`);
+ALTER TABLE `borrowed_books` ADD FOREIGN KEY (`book_id`) REFERENCES `books` (`id`);
