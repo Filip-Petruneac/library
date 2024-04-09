68cc530827c98b980b2645a17bd484b9eec3927d
Filip-Petruneac <116115251+Filip-Petruneac@users.noreply.github.com>
2024-04-09 16:22:50 +0300

    UPDATE SCHEMA.SQL

--- schema.sql
+++ schema.sql
@@ -1,8 +1,7 @@
 CREATE TABLE `authors` (
   `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
-  `Firstname` VARCHAR(255),
-  `Lastname` VARCHAR(255)
-  
+  `Lastname` VARCHAR(255),
+  `Firstname` VARCHAR(255)
 );
 
 CREATE TABLE `authors_books` (
@@ -16,28 +15,27 @@ CREATE TABLE `books` (
   `photo` VARCHAR(255),
   `title` VARCHAR(255) NOT NULL,
   `author_id` INTEGER NOT NULL,
-  `description` TEXT COMMENT 'Content of the post',
-  -- `subscriber_id` INTEGER,
+  `details` TEXT COMMENT 'Content of the post',
+  `subscriber_id` INTEGER,
   `is_borrowed` BOOLEAN DEFAULT FALSE
 );
 
 
 CREATE TABLE `subscribers` (
   `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
-  `Firstname` VARCHAR(255),
   `Lastname` VARCHAR(255),
+  `Firstname` VARCHAR(255),
   `Email` VARCHAR(255)
 );
 
 CREATE TABLE `borrowed_books` (
-  `id` INTEGER PRIMARY KEY,
   `subscriber_id` INTEGER,
   `book_id` INTEGER,
-  `subscription_date` TIMESTAMP,
+  `date_of_borrow` TIMESTAMP,
   `return_date` TIMESTAMP
 );
 
 ALTER TABLE `books` ADD FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`);
--- ALTER TABLE `books` ADD FOREIGN KEY (`is_borrowed`) REFERENCES `subscribers` (`id`);
+ALTER TABLE `books` ADD FOREIGN KEY (`is_borrowed`) REFERENCES `subscribers` (`id`);
 ALTER TABLE `borrowed_books` ADD FOREIGN KEY (`subscriber_id`) REFERENCES `subscribers` (`id`);
 ALTER TABLE `borrowed_books` ADD FOREIGN KEY (`book_id`) REFERENCES `books` (`id`);
