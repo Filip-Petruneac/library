8880ff4d42c777fc5398b98e24426045ed7cc987
Daniel Giurgiu <danielgiurgiu02@gmail.com>
2024-05-23 21:42:08 +0300

    Add Book Form

--- bff/server.py
+++ bff/server.py
@@ -26,19 +26,6 @@ def index():
     except Exception as err:
         return str(err), 500
     
-@app.route('/books')
-def books():
-    try:
-        response = requests.get(f"{API_URL}/books")
-        if response.status_code != 200:
-            return "Error fetching books from API", 400
-
-        books = response.json()
-        return render_template('books.html', books=books)
-
-    except Exception as err:
-        return str(err), 500
-
 @app.route('/book-details/<int:book_id>')
 def book_details(book_id):
     try:
@@ -139,6 +126,67 @@ def add_author():
             return jsonify(success=False, error=str(err)), 500
         
     return render_template('add_author_form.html')
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        description = request.form.get('description')
+        author_id = request.form.get('author')
+        is_borrowed = request.form.get('is_borrowed', 'off') == 'on'
+        photo = request.files['photo']
+
+        if photo:
+            filename = secure_filename(photo.filename)
+            photo_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            photo.save(photo_path)
+            photo_path = f'photos/{filename}'
+        else:
+            photo_path = None
+
+        try:
+            author_id = int(author_id)
+        except ValueError:
+            return jsonify(success=False, error="Author ID must be an integer"), 400
+
+        data = {
+            'title': title,
+            'description': description,
+            'author_id': author_id, 
+            'is_borrowed': is_borrowed,
+            'photo': photo_path
+        }
+
+        app.logger.debug(f"Data to send to API: {data}")
+
+        try:
+            response = requests.post(f"{API_URL}/books/new", json=data)
+            app.logger.debug(f"API Response: {response.status_code}, Content: {response.content}")
+
+            if response.status_code == 200: 
+                response_data = response.json()
+                app.logger.debug(f"Book added successfully: {response_data}")
+                return redirect(url_for("index"))
+            else:
+                error_message = response.json().get('error', 'Failed to add book')
+                app.logger.error(f"Failed to add book: {error_message}")
+                return jsonify(success=False, error=error_message), 500
+
+        except Exception as err:
+            app.logger.error(f"Failed to add book: {err}")
+            return jsonify(success=False, error=str(err)), 500
+
+    try:
+        response = requests.get(f"{API_URL}/authors")
+        if response.status_code != 200:
+            return "Error fetching authors from API", 400
+        authors = response.json()
+    except Exception as err:
+        app.logger.error(f"Failed to fetch authors: {err}")
+        return str(err), 500
+
+    return render_template('add_book_form.html', authors=authors)
+
 
 @app.route('/css/<path:filename>')
 def serve_css(filename):
