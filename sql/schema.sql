f49397bd42b6ab84f95d812e83699a43e2235e67
ptruneac <filippetruneac03@gmail.com>
2024-09-05 14:44:12 +0300

    Modify sql schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,19 +5,22 @@ CREATE TABLE `authors` (
   `photo` VARCHAR(255)
 );
 
-CREATE TABLE `authors_books` (
-  `id` INTEGER PRIMARY KEY,
-  `author_id` INTEGER,
-  `book_id` INTEGER
-);
-
 CREATE TABLE `books` (
   `id` INTEGER AUTO_INCREMENT PRIMARY KEY,
   `photo` VARCHAR(255),
   `title` VARCHAR(255) NOT NULL,
   `author_id` INTEGER NOT NULL,
-  `details` BIT TEXT COMMENT 'Content of the post',
-  `is_borrowed` BOOLEAN DEFAULT FALSE
+  `details` TEXT COMMENT 'Content of the post',
+  `is_borrowed` BOOLEAN DEFAULT FALSE,
+  FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `authors_books` (
+  `author_id` INTEGER,
+  `book_id` INTEGER,
+  PRIMARY KEY (`author_id`, `book_id`),
+  FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `subscribers` (
@@ -31,7 +34,9 @@ CREATE TABLE `borrowed_books` (
   `subscriber_id` INTEGER,
   `book_id` INTEGER,
   `date_of_borrow` TIMESTAMP,
-  `return_date` TIMESTAMP
+  `return_date` TIMESTAMP,
+  FOREIGN KEY (`subscriber_id`) REFERENCES `subscribers` (`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `users` (
@@ -39,8 +44,3 @@ CREATE TABLE `users` (
   `email` VARCHAR(100) UNIQUE NOT NULL,
   `password` TEXT NOT NULL
 );
-
-ALTER TABLE `books` ADD FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`);
-ALTER TABLE `books` ADD FOREIGN KEY (`is_borrowed`) REFERENCES `subscribers` (`id`);
-ALTER TABLE `borrowed_books` ADD FOREIGN KEY (`subscriber_id`) REFERENCES `subscribers` (`id`);
-ALTER TABLE `borrowed_books` ADD FOREIGN KEY (`book_id`) REFERENCES `books` (`id`);
